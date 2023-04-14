66ffdf3fb9363d4b070e0d183231d2e15d31fe7d
TechnicalDC <chauhandilip305@gmail.com>
2023-04-14 23:32:45 +0530

    initial commit

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"encoding/xml"
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hey")
+	url := "https://lukesmith.xyz/rss"
+	fmt.Println(url)
+	var body xml.Decoder
+	name := xml.Name { }
+
+	item := xml.StartElement{
+		Name `xml:"item`,
+	}
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		body.DecodeElement(resp.Body, &item)
+		fmt.Println(body)
+	}
+	defer resp.Body.Close()
+
 }
